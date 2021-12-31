fb6bafd094a86191549ef2452a755e00ad26d676
Raul Quispe <relaxedchild@gmail.com>
2021-12-30 19:57:16 -0500

    test GET /

---
+++ routes.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+
+const app = require('./app');
+
+describe('GET /', () => {
+  test('Responds with 200 status code and a message', async () => {
+    const response = await request(app).get('/');
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toEqual('Hello world - Products API');
+  });
+});
