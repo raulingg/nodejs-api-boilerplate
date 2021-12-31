909be912b0cfeab3c89aa0b51d761fdeff8ca071
Raul Quispe <relaxedchild@gmail.com>
2021-12-30 19:28:44 -0500

    create an error handler

---
+++ utils/errorHandler.js
@@ -0,0 +1,16 @@
+const { logger } = require('./logger');
+
+const isOperationalError = (err) => err.isOperational === true;
+
+// TODO: email admins, send event to an error-tracking system like Sentry,...
+const handleError = (err) => {
+  const isOperational = isOperationalError(err);
+  logger.error(err, 'Error message from the centralized error-handling component');
+
+  return isOperational;
+};
+
+module.exports.errorHandler = {
+  handleError,
+  isOperationalError,
+};
