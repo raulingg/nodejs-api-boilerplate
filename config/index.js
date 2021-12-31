17def6fe87954d6ac81ff0ed1dd38133564efa14
Raul Quispe <relaxedchild@gmail.com>
2021-12-31 12:25:07 -0500

    create an environment-based configuration

---
+++ config/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+
+const inDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  app: {
+    env: process.env.NODE_ENV || 'development',
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || 3000,
+  },
+  cors: {
+    origin: inDevelopment ? true : process.env.CORS_ORIGINS?.split(',') || false,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: false,
+  },
+  db: {
+    uri: process.env.DATABASE_URI || ':memory:',
+  },
+};
