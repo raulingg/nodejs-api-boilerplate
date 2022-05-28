f8672642cbd4787dbc1395e6c6f6fb3f61441db6
Raul Mendez <relaxedchild@gmail.com>
2022-05-28 13:36:00 -0500

    bring jest config back

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  verbose: true,
-  testEnvironment: 'node',
+  extends: ['airbnb-base', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
+  plugins: ['jest'],
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  rules: {
+    'global-require': 0,
+    'no-underscore-dangle': 0,
+  },
 };
