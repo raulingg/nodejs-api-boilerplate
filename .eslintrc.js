b1a6d0c49c72af3a0426bf1562de3d13f76e70c4
Raul Mendez <relaxedchild@gmail.com>
2022-06-06 13:17:18 -0500

    support es2020 for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,10 @@ module.exports = {
   extends: ['airbnb-base', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
   env: {
     node: true,
-    es2021: true,
+    es2020: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
   },
   rules: {
     'global-require': 0,
