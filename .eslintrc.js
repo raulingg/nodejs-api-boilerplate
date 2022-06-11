5483a6c81a0155ab49a8d5d6a49e52a3c0c3e1d9
Raul Mendez <relaxedchild@gmail.com>
2022-06-11 13:06:12 -0500

    fix prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  extends: ["airbnb-base", "plugin:jest/recommended", "prettier"],
-  plugins: ["prettier"],
+  extends: [
+    "airbnb-base",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+  ],
   env: {
     node: true,
     es2020: true,
@@ -12,8 +15,5 @@ module.exports = {
     "global-require": 0,
     "no-underscore-dangle": 0,
     "no-return-await": 0,
-    "prettier/prettier": "error",
-    "arrow-body-style": "off",
-    "prefer-arrow-callback": "off",
   },
 };
